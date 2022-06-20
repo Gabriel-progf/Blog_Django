1b892a428d75ca1069032bf82d8d0d7260063e29
Gabriel Macedo <gabriel.ol.macedo@hotmail.com>
2022-06-20 17:51:04 -0300

    First commit

---
+++ categorias/admin.py
@@ -0,0 +1,10 @@
+from unicodedata import category
+from django.contrib import admin
+from .models import Categoria
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_cat')
+    list_display_links = ('id', 'nome_cat')
+
+
+admin.site.register(Categoria, CategoriaAdmin)  
---
+++ comentarios/forms.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from .models import Comentario
+
+
+class FormComentario(ModelForm):
+    def clean(self):
+        data = self.cleaned_data
+        nome = data.get('nome_comentario')
+        email = data.get('email_comentario')
+        comentario = data.get('comentario')
+
+        if len(nome) < 3:
+            self.add_error('nome_comentario', 'O nome deve ter no mínimo 3 caracteres.')
+
+              
+    class Meta:
+        model = Comentario
+        fields = ('nome_comentario','email_comentario','comentario')
+        context_obeject_name = 'post'
